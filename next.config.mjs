894202c3ed2abcdb0fdada5d04944fac1ba109a1
Adil Arif <arifadil739@gmail.com>
2024-07-29 21:28:03 +0500

    url-update

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    BASE_URL: 'https://currency-be.vercel.app/'
+    BASE_URL: 'https://currency-be.vercel.app'
     // BASE_URL: 'http://localhost:3002'
   }
 };
